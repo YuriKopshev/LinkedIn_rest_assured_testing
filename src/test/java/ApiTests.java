5f2a59aa13dd097a618f4ad8ae72fcba16b8e0d1
Yuri Kopshev <yura.kopshev@gmail.com>
2023-03-12 20:06:39 +0200

    Initial commit

---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,84 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+
+    @Test
+    public void getCategories() {
+        String endpoint = "http://localhost:80/api_testing/category/read.php";
+        var response = given().when().get(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void getProducts() {
+        String endpoint = "http://localhost:80/api_testing/product/read.php";
+        var response = given().when().get(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void getProduct() {
+        String endpoint = "http://localhost:80/api_testing/product/read_one.php";
+        var response = given().
+                queryParam("id", 19).
+                when().
+                get(endpoint).then().statusCode(200);
+        response.log().body();
+    }
+
+    @Test
+    public void getProductsStatus() {
+        String endpoint = "http://localhost:80/api_testing/product/read.php";
+        var response = given().when().get(endpoint).then().statusCode(200);
+        response.log().status();
+    }
+
+    @Test
+    public void createProduct() {
+        String endpoint = "http://localhost:80/api_testing/product/create.php";
+        String body = """
+                {
+                "name": "Water Bottle",
+                "description": "Blue water bottle. Holds 64 ounces",
+                "price": 12,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().post(endpoint).then().statusCode(201);
+        response.log().body();
+        response.log().headers();
+    }
+
+    @Test
+    public void updateProduct() {
+        String endpoint = "http://localhost:80/api_testing/product/update.php";
+        String body = """
+                {
+                "id": 19,
+                "name": "Water Bottle",
+                "description": "Blue water bottle. Holds 64 ounces",
+                "price": 15,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().put(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void deleteProduct() {
+        String endpoint = "http://localhost:80/api_testing/product/delete.php";
+        String body = """
+                {
+                "id": 19
+                }
+                """;
+        var response = given().body(body).when().delete(endpoint).then();
+        response.log().body();
+    }
+}
+
---
+++ src/test/java/LessonNewProductTests.java
@@ -0,0 +1,77 @@
+import org.example.Product;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class LessonNewProductTests {
+
+    @Test
+    public void createSweatBand() {
+        String endpoint = "http://localhost:80/api_testing/product/create.php";
+
+        Product product = new Product(
+                "Sweatband",
+                "Good sweetband",
+                5.0,
+                3
+        );
+        var response = given().
+                body(product).
+                when().
+                post(endpoint).
+                then().
+                statusCode(201);
+        response.log().status();
+        response.log().body();
+    }
+
+    @Test
+    public void updateSweatBand() {
+        String endpoint = "http://localhost:80/api_testing/product/update.php";
+        String body = """
+                 {
+                "id": 22,
+                "price": 6.0,
+                }
+                """;
+        var response = given().
+                body(body).
+                when().
+                put(endpoint).
+                then().
+                statusCode(200);
+        response.log().status();
+        response.log().body();
+    }
+
+    @Test
+    public void getSweetBand() {
+        String endpoint = "http://localhost:80/api_testing/product/read_one.php";
+        var response = given().
+                queryParam("id", 22).
+                when().
+                get(endpoint).
+                then().
+                statusCode(200);
+        response.log().status();
+        response.log().body();
+    }
+
+    @Test
+    public void deleteSweetBand() {
+        String endpoint = "http://localhost:80/api_testing/product/delete.php";
+        String body = """
+                {
+                "id": 22
+                }
+                """;
+        var response = given().
+                body(body).
+                when().
+                delete(endpoint).
+                then().
+                statusCode(200);
+        response.log().status();
+        response.log().body();
+    }
+}
